e023a6edc43bb25748d729de5fce20ad63563784
zhang meng <meng.2.zhang@uconn.edu>
2023-03-09 13:41:29 -0500

    new version

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite'
 import reactRefresh from '@vitejs/plugin-react-refresh'
 
-// https://vitejs.dev/config/
+// https://vitejs.dev/config/git
 export default defineConfig({
   plugins: [reactRefresh()],
   build: {
